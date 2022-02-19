f597b521ab9ea14f011b337fe10eefd5cc91356a
Esmond Lam <esmondlam0923@gmail.com>
2022-02-19 22:35:21 +0800

    feat: basic layout

---
+++ src/Component/Main.jsx
@@ -0,0 +1,20 @@
+import Die from './Die'
+
+export default function Main() {
+    return (
+        <main >
+            <div className='container-die'>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+                <Die value="2"/>
+            </div>
+        </main>
+    )
+}
