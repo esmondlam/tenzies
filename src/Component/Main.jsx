c6bb540fb1d14083ccba04205cbde7d5346c6a39
Esmond Lam <esmondlam0923@gmail.com>
2022-02-20 17:16:43 +0800

    feat(Main.jsx): add hold-dice logic

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,25 +1,59 @@
 import Die from './Die'
 import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 export default function Main() {
-    
+
     const [dice, setDice] = useState(allNewDice())
 
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            })
         }
         return newDice
     }
 
-    const diceElms = dice.map(dice => <Die value={dice} />)
+    function rollDice() {
+        setDice(prevDice => prevDice.map(die => (
+            die.isHeld === true
+            ? die
+            : {
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                id: nanoid()
+            }
+        )))
+    }
+
+    function holdDice(id) {
+        setDice(prevDice => prevDice.map(die => (
+            die.id === id 
+            ? {...die, isHeld: !die.isHeld}
+            : die
+        )))
+    }
+
+    const diceElms = dice.map(dice => <Die 
+        key={dice.id}
+        value={dice.value} 
+        isHeld={dice.isHeld}
+        holdDice={()=>holdDice(dice.id)}
+        />)
 
     return (
         <main >
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            <br/>Click each die to freeze it at its current value between rolls.</p>
             <div className='container-die'>
                 {diceElms}
             </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
