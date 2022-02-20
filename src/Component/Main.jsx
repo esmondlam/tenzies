2c3902ba18ba7ee79630c553aa76758fa625d76d
Esmond Lam <esmondlam0923@gmail.com>
2022-02-20 11:03:03 +0800

    feat(Main.jsx): make random-num dice

--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,19 +1,24 @@
 import Die from './Die'
+import React, { useState } from 'react'
 
 export default function Main() {
+    
+    const [dice, setDice] = useState(allNewDice())
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    const diceElms = dice.map(dice => <Die value={dice} />)
+
     return (
         <main >
             <div className='container-die'>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
-                <Die value="2"/>
+                {diceElms}
             </div>
         </main>
     )
